cb9b4338218c729d998d58ad647f8e8bd01c25f2
Alvin Sanchez <alvin.sanchez27@gmail.com>
2022-07-02 14:10:09 -0400

    First commit

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+
+names = [];
+eventName = "";
+
+function writeCards(names, eventName){
+    for(let i = 0; i < names.length; i++){
+    console.log(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+}
+
+let number = 10;
+function countDown(number){
+while (countDown < 10) {
+    console.log(countDown--)
+}
+}
+countDown(10)
